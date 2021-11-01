81ccce6cd2ce2b338b4b44e891bcaa8dca72a17b
Luis Fominaya <fnn.fpluis@gmail.com>
2021-11-01 17:16:45 +0100

    Move contract deployment to Moralis on Kovan

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -24,7 +24,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "kovan";
 
 const mainnetGwei = 21;
 
@@ -68,9 +68,9 @@ module.exports = {
       },
     },
     kovan: {
-      url: "https://kovan.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      // url: "https://kovan.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
 
-      //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/kovan", // <---- YOUR MORALIS ID! (not limited to infura)
+      url: "https://speedy-nodes-nyc.moralis.io/3e19e4f18edcb50276edb58f/eth/kovan",
 
       accounts: {
         mnemonic: mnemonic(),
